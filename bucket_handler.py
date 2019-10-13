2c8fe4c10e9ee9d7c24056d232c3b05cc030e765
HubertStefanski <hubertstefanski@gmail.com>
2019-10-13 23:42:26 +0100

    bucket_handler pull_resource function WIP

--- bucket_handler.py
+++ bucket_handler.py
@@ -2,15 +2,22 @@
 import sys
 import boto3
 from termcolor import colored
+import requests
+#from urllib3.request import urlretrieve
 
-
+#create new s3 Bucket with timestamp as UID
 def createBucket(bucketName):
     s3 = boto3.resource("s3")
     print(colored('Creating new Bucket','blue'))
     try:
-        response = s3.create_bucket(Bucket=bucketName, CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'})
+        response = s3.create_bucket(Bucket='web-server-bucket-' + bucketName, CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'})
         print (colored(response, 'yellow'))
     except Exception as error:
         print (colored(error,'red'))
         
 
+def pullImageFromURL(url):
+    try:
+        requests.get(url,'local_image.jpg')
+    except FileNotFoundError as error:
+        print(error)
--- run_newwebserver.py
+++ run_newwebserver.py
@@ -13,7 +13,7 @@ import sys
 
 # Import instance and bucket handlers 
 import instance_handler
-import bucket_handler
+from bucket_handler import *
 
 # Import for feedback highlighting
 from termcolor import colored
@@ -23,6 +23,13 @@ import time
 
 # Create a timestamp to give UID to each component
 timestamp = time.strftime("%Y%m%d-%H%M")
+
+# Set Url var to argument from command else
+try:
+    resourceURL = sys.argv[1]
+except IndexError:
+    print(colored('resource not found, setting to default image','red'))
+    resourceURL = "http://www.infinitecat.com/archive18/cute18/finger-on-cat-nose.jpg"
 
 # Perform internet connectivity check
 def checkConnectionToResource(url):
@@ -36,7 +43,7 @@ def checkConnectionToResource(url):
             return False
    
     if(sendRequest(url)== True ):
-         print(colored('Internet connection is Up','green'))
+         print(colored('Internet connection to: ' + url +' is up','green'))
 
     if(sendRequest(url) == False):
         print(colored('Internet connection to : ' + url + ' is down Ensure Internet connectivity','red', attrs=['bold']))   
@@ -47,7 +54,10 @@ def checkConnectionToResource(url):
 checkConnectionToResource('http://google.com/') 
 
 # Call bucket handler to create new Bucket
-bucket_handler.createBucket('web_bucket' + timestamp )
+checkConnectionToResource(resourceURL)
+createBucket(timestamp)
+print(colored('Pulling image down from : ' + resourceURL,'cyan'))
+pullImageFromURL(resourceURL)
 
 
 # Call instance handler to create new instance and run http server
